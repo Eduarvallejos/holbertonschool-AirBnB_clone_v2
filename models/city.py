b3329280757424b660e47f254fc07c43a6d040ea
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-01-24 15:58:39 -0500

    update

--- models/city.py
+++ models/city.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
+from os import getenv
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    class City(BaseModel, Base):
+        """This class defines a City by various attributes"""
+        __tablename__ = "cities"
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
 
-class City(BaseModel, Base):
-    """This class defines a City by various attributes"""
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        """represents a relationship with the class Places"""
+        places = relationship("Place", backref="cities", cascade="delete")
 
-    """represents a relationship with the class Places"""
-    places = relationship("Place", backref="cities", cascade="delete")
+else:
+    class City(BaseModel):
+        """ The city class, contains state ID and name """
+        name = ''
+        state_id = ''

b1f2c215870a5a6c96cb2ed962d91c79d9ee0038
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-01-23 23:10:15 -0500

    update

--- models/place.py
+++ models/place.py
@@ -6,9 +6,9 @@ from models.amenity import Amenity
 from sqlalchemy import Column, Table,  String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 
-place_amenity = Table('place_amenity', Base.metadata,
-    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
-    Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
+place_amenity = Table("place_amenity", Base.metadata,
+    Column("place_id", String(60), ForeignKey("places.id"), primary_key=True, nullable=False),
+    Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False)
 )
 
 class Place(BaseModel, Base):

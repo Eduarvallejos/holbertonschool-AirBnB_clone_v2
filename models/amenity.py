58f052764f84781177f41bf364ac45add4309493
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-01-24 15:25:24 -0500

    update

--- models/amenity.py
+++ models/amenity.py
@@ -2,9 +2,18 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from os import getenv
 
+if getenv('HBNB_TYPE_STORAGE') == 'db':
 
-class Amenity(BaseModel, Base):
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
+    class Amenity(BaseModel, Base):
+        """Class Amenity for DB"""
+        name = ''
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+
+else:
+    class Amenity(BaseModel):
+        """Class Amenity for FS"""
+
+        name = ''

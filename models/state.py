d9801286f1a91ef5b3f792d6e95aaed9642b5d41
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-01-24 15:53:54 -0500

    update

--- models/state.py
+++ models/state.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from os import getenv
 from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    class State(BaseModel, Base):
+        """ State class """
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
 
-class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
+        # For DBStorage
+        cities = relationship("City", backref="state", cascade="all, delete-orphan")
 
-    # For DBStorage
-    cities = relationship("City", backref="state", cascade="all, delete-orphan")
-
-    # For FileStorage
-    @property
-    def cities(self):
-        from models import storage
-        cities = []
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                cities.append(city)
-        return cities
+else:
+    class State(BaseModel):
+        """State class for FS"""
+        name = ''
+        # For FileStorage
+        @property
+        def cities(self):
+            from models import storage
+            cities = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    cities.append(city)
+            return cities

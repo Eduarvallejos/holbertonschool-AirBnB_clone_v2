fc01fa90be0395f5201386fca947b82428c70282
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-01-24 15:08:56 -0500

    update

--- models/place.py
+++ models/place.py
@@ -30,8 +30,8 @@ class Place(BaseModel, Base):
 
     # For DBStorage
     reviews = relationship("Review", backref="place", cascade="all, delete-orphan")
-    amenities = relationship("Amenity", secondary="place_amenity",
-                             backref="place_amenities",viewonly=False)
+    amenities = relationship("Amenity", secondary=place_amenity,
+                             back_populates="place_amenities",viewonly=False)
 
     # For FileStorage
     @property

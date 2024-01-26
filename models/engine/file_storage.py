d37cd20b73015dc01aa299e97cbbe64e0ab06a33
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-01-25 22:28:18 -0500

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -35,9 +35,7 @@ class FileStorage:
         """Loads storage dictionary from file"""
         from models.base_model import BaseModel
         from models.user import User
-        print("Before import Place")
         from models.place import Place
-        print("After import Place")
         from models.state import State
         from models.city import City
         from models.amenity import Amenity

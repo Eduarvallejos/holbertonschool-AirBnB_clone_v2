73e6488d89e0463f374c87bf2cf8238d3d99c9bc
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-01-26 21:37:52 -0500

    update

--- models/city.py
+++ models/city.py
@@ -5,11 +5,18 @@ from os import getenv
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
-class City(BaseModel, Base):
-    """This class defines a City by various attributes"""
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    class City(BaseModel, Base):
+        """This class defines a City by various attributes"""
+        __tablename__ = "cities"
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
 
-    """represents a relationship with the class Places"""
-    places = relationship("Place", backref="cities", cascade="delete")
+        """represents a relationship with the class Places"""
+        places = relationship("Place", backref="cities", cascade="delete")
+
+else:
+    class City(BaseModel):
+        """ The city class, contains state ID and name """
+        name = ''
+        state_id = ''
--- models/user.py
+++ models/user.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel,Base
+from os import getenv
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+if getenv('HBNB_TYPE_STORAGE') == 'db':
 
-class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    class User(BaseModel, Base):
+        """This class defines a user by various attributes"""
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
 
-    """represents a relationship with the class Places"""
-    places = relationship("Place", backref="user", cascade="all, delete-orphan")
+        """represents a relationship with the class Places"""
+        places = relationship("Place", backref="user",
+                              cascade="all, delete-orphan")
 
-    """represents a relationship with the class Review"""
-    reviews = relationship("Review", backref="user", cascade="all, delete-orphan")
+        """represents a relationship with the class Review"""
+        reviews = relationship("Review", backref="user",
+                               cascade="all, delete-orphan")
+        
+else:
+    class User(BaseModel):
+        """This class defines a usar by various attributes FS"""
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''

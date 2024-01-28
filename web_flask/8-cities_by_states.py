f8cb5598318c1c7c9380a95d479bd81530d4028f
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-01-27 20:13:39 -0500

    list the states and their cities

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This script lists all objects of the state class
+present in the database.
+"""
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+"""
+Close the connection to the database
+terminate each Flask application context.
+"""
+@app.teardown_appcontext
+def teardonw_db():
+    storage.close()
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """
+    Gets all objects of the State class from
+    the database.
+    """
+    state_list = storage.all(State).values()
+    # Render the template with the list of states and cities
+    return render_template("8-cities_by_states.html", states=state_list)
+
+
+if __name__ == "__main":
+    """
+    Start the Flask application so that it listens on all
+    the interfaces on port 5000.
+    """
+    app.run(host="0.0.0.0", port=5000)

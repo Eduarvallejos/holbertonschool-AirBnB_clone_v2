0ca423cadf2c9732e0359fce816102a314c54a33
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-01-27 20:56:46 -0500

    States class objects

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+"""
+This script lists all objects of the state class
+present in the database.
+"""
 from models import storage
 from models.state import State
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+"""
+Close the connection to the database
+terminate each Flask application context.
+"""
 
 
 @app.teardown_appcontext
@@ -12,9 +21,17 @@ def teardonw_db():
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
+    """
+    Gets all objects of the State class from
+    the database.
+    """
     state_list = storage.all(State).values()
     return render_template("7-states_list.py", states=state_list)
 
 
 if __name__ == "__main":
+    """
+    Start the Flask application so that it listens on all
+    the interfaces on port 5000.
+    """
     app.run(host="0.0.0.0", port=5000)
